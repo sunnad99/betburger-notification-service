54cee86c7f47e5272124299843ebf31f9393d2e7
Sulaiman Nadeem <s.nadeem@prismware.co>
2024-03-08 20:26:25 +0500

    feat: Add logging configuration and changed format for message

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
 import pytz
+import logging
+
+# Logging
+LOG_LEVEL = logging.INFO
+LOG_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 
 URL_MAPPING = {19: "https://www.unibet.com/betting/sports/event/{bookmaker_event_link}"}
 
@@ -8,16 +13,13 @@ MIN_ODDS_FACTOR = 0.874
 TIME_ZONE = pytz.timezone("Europe/Stockholm")
 
 BASE_MESSAGE = """
-    {league_name}
-
 ⚽️ {event_name}
-
 🎲 Bets
 {bets}
 
+
+{league_name}
 🔐 Lägsta spelbara odds {min_odds}
-
 🕰️  {match_time}
-
 🌐 {bet_url}
 """
