8fbafb811f79b868091382e06a41a01847cc2509
Sulaiman Nadeem <s.nadeem@prismware.co>
2024-03-08 20:56:33 +0500

    feat: Add FREQUENCY_MINUTES constant

--- config.py
+++ config.py
@@ -8,6 +8,7 @@ LOG_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 URL_MAPPING = {19: "https://www.unibet.com/betting/sports/event/{bookmaker_event_link}"}
 
 MIN_ODDS_FACTOR = 0.874
+FREQUENCY_MINUTES = 1
 
 
 TIME_ZONE = pytz.timezone("Europe/Stockholm")
