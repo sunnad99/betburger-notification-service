9d2dde42b797bdc9b5c5962ddc48a602f4e2c7a5
Sulaiman Nadeem <s.nadeem@prismware.co>
2024-03-08 21:19:35 +0500

    feat: Update FREQUENCY_MINUTES to 2

--- config.py
+++ config.py
@@ -1,14 +1,9 @@
 import pytz
-import logging
-
-# Logging
-LOG_LEVEL = logging.INFO
-LOG_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 
 URL_MAPPING = {19: "https://www.unibet.com/betting/sports/event/{bookmaker_event_link}"}
 
 MIN_ODDS_FACTOR = 0.874
-FREQUENCY_MINUTES = 1
+FREQUENCY_MINUTES = 2
 
 
 TIME_ZONE = pytz.timezone("Europe/Stockholm")
