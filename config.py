0d40af0210a70a3165f5833e30cf8e9968ce70ff
Sulaiman Nadeem <s.nadeem@prismware.co>
2024-03-14 20:52:51 +0500

    feat: Add logging configuration to config.py

--- config.py
+++ config.py
@@ -1,4 +1,10 @@
 import pytz
+import logging
+
+# Initialize the logger
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 URL_MAPPING = {19: "https://www.unibet.com/betting/sports/event/{bookmaker_event_link}"}
 
