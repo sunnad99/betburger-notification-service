efbe38bd2ad9c29c38bccc823374123f8baae921
Sulaiman Nadeem <s.nadeem@prismware.co>
2024-03-14 22:38:22 +0500

    feat: Refactor credentials loading and add config file support

--- credentials.py
+++ credentials.py
@@ -1,9 +1,21 @@
 import os
+import json
 from dotenv import load_dotenv
 
 load_dotenv()
 
-TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")
-TELEGRAM_CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID")
-BET_BURGER_TOKEN = os.environ.get("BET_BURGER_TOKEN")
-BET_BURGER_FILTER_ID = os.environ.get("BET_BURGER_FILTER_ID")
+
+CONFIG_FILENAME = os.environ.get("CONFIG_FILENAME", "bot_test_config.json")
+
+# Load the credentials from the config file
+CREDS = {}
+with open(CONFIG_FILENAME, "r") as f:
+    CREDS = json.load(f)
+
+# Load the credentials for telegram
+TELEGRAM_TOKEN = CREDS.get("TELEGRAM_AUTH_TOKEN")
+TELEGRAM_CHAT_MAPPING = CREDS.get("TELEGRAM_CHAT_MAPPING", {})
+
+# Load the credentials for betburger
+BET_BURGER_TOKEN = CREDS.get("BET_BURGER_TOKEN")
+BET_BURGER_FILTER_ID = CREDS.get("BET_BURGER_FILTER_ID")
