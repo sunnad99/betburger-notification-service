82e66d776dfcbbb086976dac5338041dd61ce37b
Sulaiman Nadeem <s.nadeem@prismware.co>
2024-04-02 17:22:40 +0500

    feat: Add subscription cancellation functionality

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,7 +1,9 @@
 import logging
 import datetime
 import selector
+import deleter
 import pandas as pd
+import stripe
 
 from telegram import (
     Update,
@@ -13,13 +15,12 @@ from telegram.ext import (
     ApplicationBuilder,
     ContextTypes,
     CommandHandler,
-    MessageHandler,
     CallbackQueryHandler,
-    filters,
 )
 
-from credentials import TELEGRAM_TOKEN
+from credentials import TELEGRAM_TOKEN, PAYMENT_PROVIDER_TOKEN
 
+stripe.api_key = PAYMENT_PROVIDER_TOKEN
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
@@ -67,7 +68,57 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     )
 
 
-async def handle_subscription(
+async def cancel_subscription(
+    update: Update, context: ContextTypes.DEFAULT_TYPE
+) -> None:
+    """
+    Cancel the subscription process for a product
+
+    Args:
+        update: Update - The update object
+        context: ContextTypes.DEFAULT_TYPE - The context object
+
+    Returns:
+        None
+    """
+    user_id = update.effective_user.id
+    customers = selector.get_customers(telegram_user_id=user_id)
+
+    if not customers:
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text="You don't have any active subscriptions.",
+        )
+        return
+
+    customer = customers[0]
+    subscriptions = selector.get_subscriptions(customer_id=customer["id"])
+    if not subscriptions:
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text="You don't have any active subscriptions.",
+        )
+        return
+
+    options = []
+    for subscription in subscriptions:
+        product = selector.get_products(product_id=subscription["product_id"])[0]
+        button = [
+            InlineKeyboardButton(
+                f"Cancel {product['name'].title()} subscription",
+                callback_data=subscription["stripe_subscription_id"],
+            )
+        ]
+        options.append(button)
+
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Please select the subscription you want to cancel.",
+        reply_markup=InlineKeyboardMarkup(options),
+    )
+
+
+async def handle_start_subscription(
     update: Update, context: ContextTypes.DEFAULT_TYPE
 ) -> None:
     """
@@ -138,17 +189,160 @@ async def handle_subscription(
     )
 
 
+async def confirm_subscription_cancellation(
+    update: Update, context: ContextTypes.DEFAULT_TYPE
+) -> None:
+    """
+    Confirm the subscription cancellation process for a product
+
+    Args:
+        update: Update - The update object
+        context: ContextTypes.DEFAULT_TYPE - The context object
+
+    Returns:
+        None
+    """
+
+    query = update.callback_query
+    stripe_subscription_id = query.data
+
+    existing_subscription = selector.get_subscriptions(
+        stripe_subscription_id=stripe_subscription_id
+    )[0]
+    existing_product = selector.get_products(
+        product_id=existing_subscription["product_id"]
+    )[0]
+
+    # Confirm the subscription cancellation
+    await query.answer()
+    await query.edit_message_text(
+        text=f"Are you sure you want to cancel your {existing_product['name'].title()} subscription?",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        text="Yes",
+                        callback_data=f"confirm_{stripe_subscription_id}",
+                    ),
+                    InlineKeyboardButton(
+                        text="No",
+                        callback_data=f"cancel_{stripe_subscription_id}",
+                    ),
+                ],
+            ]
+        ),
+    )
+
+
+async def handle_end_subscription(
+    update: Update, context: ContextTypes.DEFAULT_TYPE
+) -> None:
+    """
+    Handle the end subscription process for a product
+
+    Args:
+        update: Update - The update object
+        context: ContextTypes.DEFAULT_TYPE - The context object
+
+    Returns:
+        None
+    """
+
+    query = update.callback_query
+    stripe_subscription_id = query.data.replace("confirm_", "")
+
+    existing_subscription = selector.get_subscriptions(
+        stripe_subscription_id=stripe_subscription_id
+    )[0]
+    existing_product = selector.get_products(
+        product_id=existing_subscription["product_id"]
+    )[0]
+
+    # Cancel the subscription from stripe and remove it from the database
+    stripe.Subscription.delete(stripe_subscription_id)
+    deleter.delete_subscription(stripe_subscription_id)
+
+    # Let the user know that the subscription has been cancelled
+    await query.answer()
+    await query.edit_message_text(
+        text=f"Your {existing_product['name'].title()} subscription has been cancelled."
+    )
+
+
+async def cancel_subscription_cancellation(
+    update: Update, context: ContextTypes.DEFAULT_TYPE
+) -> None:
+    """
+    Cancel the subscription cancellation process for a product
+
+    Args:
+        update: Update - The update object
+        context: ContextTypes.DEFAULT_TYPE - The context object
+
+    Returns:
+        None
+    """
+
+    query = update.callback_query
+    stripe_subscription_id = query.data.replace("cancel_", "")
+
+    existing_subscription = selector.get_subscriptions(
+        stripe_subscription_id=stripe_subscription_id
+    )[0]
+    existing_product = selector.get_products(
+        product_id=existing_subscription["product_id"]
+    )[0]
+
+    # Cancel the subscription cancellation
+    await query.answer()
+    await query.edit_message_text(
+        text=f"Your {existing_product['name'].title()} subscription has not been cancelled."
+    )
+
+
 if __name__ == "__main__":
     application = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
 
-    start_handler = CommandHandler("start", start)
+    start_subscription_handler = CommandHandler("start", start)
 
-    # simple start function
-    application.add_handler(start_handler)
-
-    # Payment without shipping handler for a specific item
-    application.add_handler(
-        CallbackQueryHandler(handle_subscription, pattern=r"^price_.*$")
+    handle_start_subscription_handler = CallbackQueryHandler(
+        handle_start_subscription, pattern=r"^price_.*$"
     )
+
+    end_subscription_handler = CommandHandler("endSubscription", cancel_subscription)
+
+    confirm_subscription_cancellation_handler = CallbackQueryHandler(
+        confirm_subscription_cancellation, pattern=r"^sub_.*$"
+    )
+
+    cancel_subscription_cancellation_handler = CallbackQueryHandler(
+        cancel_subscription_cancellation, pattern=r"^cancel_sub_.*$"
+    )
+
+    handle_end_subscription_handler = CallbackQueryHandler(
+        handle_end_subscription, pattern=r"^confirm_sub_.*$"
+    )
+
+    ### Subscription process ###
+
+    # Start subscription process
+    application.add_handler(start_subscription_handler)
+
+    # Handling the start subscription process
+    application.add_handler(handle_start_subscription_handler)
+
+    ### Subscription cancellation process ###
+
+    # End subscription process
+    application.add_handler(end_subscription_handler)
+
+    # Confirming the subscription cancellation
+    application.add_handler(confirm_subscription_cancellation_handler)
+
+    # Cancelling the subscription cancellation
+    application.add_handler(cancel_subscription_cancellation_handler)
+
+    # Handling the end subscription process
+    application.add_handler(handle_end_subscription_handler)
 
     application.run_polling(allowed_updates=Update.ALL_TYPES)
